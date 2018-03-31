4671ad5d7512567e593c7c0b28d32fef906635d6
Brunovsky <elbrunovsky@gmail.com>
2018-03-31 20:42:05 +0100

    Added meta

--- CAL1718_T4GE/src/load_map.cpp
+++ CAL1718_T4GE/src/load_map.cpp
@@ -1,9 +1,26 @@
 #include "FunctionsPrototypes.h"
 #include <regex>
 #include <fstream>
+#include <numeric_limits>
 
 extern GraphViewer *gv;
 extern Graph<int> myGraph;
+
+class GraphMetaData {
+public:
+	long double min_latitude, max_latitude;
+	long double min_longitude, max_longitude;
+	int nodes, edges;
+
+	int width, height;
+
+	int getX(long double longitude) {
+		return width * (longitude - min_longitude) / (max_longitude - min_longitude);
+	}
+	int getY(long double latitude) {
+		return height * (latitude - min_latitude) / (max_latitude - min_latitude);
+	}
+};
 
 // ** Roads: road_id;road_name;two_way
 //    ^(\d+);((?:[-0-9a-zA-ZÀ-ÿ,\.]| )+);(False|True);?$
@@ -22,6 +39,9 @@ extern Graph<int> myGraph;
 //    [3] : Node 2 id
 
 
+
+
+
 // ** Node: node_id;lat_deg;long_deg;long_rad;lat_rad
 //    ^(\d+);(-?\d+.?\d*);(-?\d+.?\d*);(-?\d+.?\d*);(-?\d+.?\d*);?$
 // -> ECMAScript
@@ -29,12 +49,10 @@ extern Graph<int> myGraph;
 //    [1] : Node id
 //    [2] : Latitude in degrees
 //    [3] : Longitude in degrees
-//    [4] : Longitude in radians
-//    [5] : Latitude in radians
-
-
-int load_nodes(string filename) {
-	constexpr std::regex reg("^(\\d+);(-?\\d+.?\\d*);(-?\\d+.?\\d*);(-?\\d+.?\\d*);(-?\\d+.?\\d*);?$");
+//    [4]*: Longitude in radians
+//    [5]*: Latitude in radians
+int load_nodes(string filename, GraphMetaData meta) {
+	const std::regex reg("^(\\d+);(-?\\d+.?\\d*);(-?\\d+.?\\d*);(-?\\d+.?\\d*);(-?\\d+.?\\d*);?$");
 
 	std::ifstream file(filename);
 	if (!file.is_open()) return -1;
@@ -43,7 +61,23 @@ int load_nodes(string filename) {
 	std::getline(file, line);
 
 	while (!file.eof() && !file.fail()) {
+		std::smatch match;
+		if (std::regex_match(line, match, reg)) {
+			// Get Node ID
+			long long id = std::stoll(match[1]);
+			if (id > std::numeric_limits<int>::max()) {
+				// TODO
+			}
+			// Get Node Latitude
+			double long latitude = std::stold(match[2]);
+			int y = meta.getY(latitude);
+			// Get Node Longitude
+			double long longitude = std::stold(match[3]);
+			int x = meta.getX(longitude);
 
+			// Add Node
+			myGraph.addVertex()
+		}
 
 		std::getline(file, line);
 	}
